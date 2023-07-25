230713dea639897718501319fced9211c3bd8270
Niels Voss <code@nielsvoss.com>
2023-07-25 12:51:00 -0700

    Rename ExampleMod to ElytraTweaksMod

--- src/client/java/osbourn/elytratweaks/ElytraTweaksModClient.java
+++ src/client/java/osbourn/elytratweaks/ElytraTweaksModClient.java
@@ -2,7 +2,7 @@ package osbourn.elytratweaks;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class ElytraTweaksModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
