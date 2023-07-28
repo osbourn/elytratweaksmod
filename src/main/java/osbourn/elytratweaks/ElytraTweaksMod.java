620a38b46c4bf75af8fd4c43030c379abf47e960
Niels Voss <code@nielsvoss.com>
2023-07-27 20:13:34 -0700

    Remove mod template comments

--- src/main/java/osbourn/elytratweaks/ElytraTweaksMod.java
+++ src/main/java/osbourn/elytratweaks/ElytraTweaksMod.java
@@ -13,9 +13,6 @@ import org.slf4j.LoggerFactory;
 import java.util.Objects;
 
 public class ElytraTweaksMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("elytratweaks");
 	public static final RegistryKey<DamageType> FLY_ON_GROUND_DAMAGE =
 			RegistryKey.of(RegistryKeys.DAMAGE_TYPE, new Identifier("elytratweaks", "fly_on_ground"));
@@ -35,10 +32,6 @@ public class ElytraTweaksMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		config = ElytraTweaksConfig.load();
 	}
 }
