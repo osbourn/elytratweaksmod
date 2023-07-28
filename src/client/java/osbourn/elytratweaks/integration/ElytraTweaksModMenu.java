e647839ccfec7b4b410ee6c094f58acd71aaccae
Niels Voss <code@nielsvoss.com>
2023-07-27 21:18:43 -0700

    Start implementing Mod Menu and Cloth Config integration

---
+++ src/client/java/osbourn/elytratweaks/integration/ElytraTweaksModMenu.java
@@ -0,0 +1,18 @@
+package osbourn.elytratweaks.integration;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import net.minecraft.text.Text;
+
+public class ElytraTweaksModMenu implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> {
+            ConfigBuilder builder = ConfigBuilder.create()
+                    .setParentScreen(parent)
+                    .setTitle(Text.literal("Elytra Tweaks Config"));
+            return builder.build();
+        };
+    }
+}
